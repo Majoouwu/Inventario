2bee91d884832a8831ce26d245a8fb95c7aa185d
Majobubulubu <mariajosejj2019@gmail.com>
2023-01-22 21:44:49 -0600

    clase usuario

--- src/Moldes/Usuarios/Usuario.java
+++ src/Moldes/Usuarios/Usuario.java
@@ -1,4 +1,19 @@
 package src.Moldes.Usuarios;
 
+import java.io.Serializable;
+import java.util.UUID;
 public class Usuario {
+    private String password;
+    private String telefono;
+    private String usuario;
+    private String nombre;
+    private UUID id;
+
+    public Usuario(String nombre, String telefono, String usuario, String password) {
+        this.id = UUID.randomUUID();
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.usuario = usuario;
+        this.password = password;
+    }
 }
