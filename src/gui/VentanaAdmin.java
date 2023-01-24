025682719cbd74fb568941c54a226bb67ed967d5
Rodsote <95106582+Rodsote@users.noreply.github.com>
2023-01-23 23:37:09 -0600

    Modified files

--- src/gui/VentanaAdmin.java
+++ src/gui/VentanaAdmin.java
@@ -1,6 +1,7 @@
 package src.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VentanaAdmin{
 
@@ -12,24 +13,32 @@ public class VentanaAdmin{
 
     public void mostrar(){
         crearVentana();
-        ponerBotones();
         ponerEtiquetas();
+        ponerBotones();
+
     }
 
     private void crearVentana(){
         ventana.setSize(1280,720);
         ventana.setVisible(true);
+
+        contentPane.setSize(1280,720);
+
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     private void ponerEtiquetas(){
-        JLabel label = new JLabel("Etiqueta");
+        JLabel label = new JLabel("Inventario: Majo Y Rodrigo S.A. de C.V.");
         contentPane.add(label);
+        label.setBounds(0,0,50,20);
+        label.setFont(new Font("Sans", Font.PLAIN, 35));
 
     }
 
     private void ponerBotones(){
-
+        JButton boton = new JButton();
+        contentPane.add(boton);
+        boton.setBounds(300,250,100,30);
     }
 
 }
