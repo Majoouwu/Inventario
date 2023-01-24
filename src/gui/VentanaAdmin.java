0c9e55a720f49fd8d047ff48dc18f46bd3fe77a5
Rodsote <95106582+Rodsote@users.noreply.github.com>
2023-01-23 23:16:35 -0600

    Modified files

--- src/gui/VentanaAdmin.java
+++ src/gui/VentanaAdmin.java
@@ -5,14 +5,31 @@ import javax.swing.*;
 public class VentanaAdmin{
 
     JFrame ventana = new JFrame();
+    JPanel contentPane = (JPanel) ventana.getContentPane();
     public VentanaAdmin(){
 
     }
 
     public void mostrar(){
-        ventana.setSize(400,500);
+        crearVentana();
+        ponerBotones();
+        ponerEtiquetas();
+    }
+
+    private void crearVentana(){
+        ventana.setSize(1280,720);
         ventana.setVisible(true);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void ponerEtiquetas(){
+        JLabel label = new JLabel("Etiqueta");
+        contentPane.add(label);
+
+    }
+
+    private void ponerBotones(){
+
     }
 
 }
