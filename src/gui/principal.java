c40919c9278af6bbb432b4a94611576da7f6d777
majobubulubu <maria.juarez@ingenieria.unam.edu>
2023-01-24 00:26:17 -0600

    comment

--- src/gui/principal.java
+++ src/gui/principal.java
@@ -1,5 +1,16 @@
 package src.gui;
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.BorderFactory;
+import java.awt.BorderLayout;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class principal {
-    
+    // HOLIIIISSSSSSS
 }
