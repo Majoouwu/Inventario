77fbce14ab5125939745a8ef29f988dc98af1afc
Majobubulubu <mariajosejj2019@gmail.com>
2023-01-23 22:35:55 -0600

    vistas de la ventana

--- src/Moldes/Productos/Anillo.java
+++ src/Moldes/Productos/Anillo.java
@@ -3,6 +3,6 @@ package src.Moldes.Productos;
 public class Anillo extends Molde {
 
     public Anillo(String material, float precio, String nombre, float peso){
-        super(material, nombre, peso, precio);
+        super(material, precio, nombre, peso);
     }
 }
--- src/gui/Vistas/Login.java
+++ src/gui/Vistas/Login.java
@@ -1,4 +1,28 @@
 package src.gui.Vistas;
 
-public class Login {
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import src.gui.principal;
+
+public class Login extends JPanel{
+
+    private JTextField campoUsuario;
+
+    private JPasswordField campoPassword;
+
+    private JCheckBox mostrarPassword;
+
+    private JButton botonLogin;
+
 }
