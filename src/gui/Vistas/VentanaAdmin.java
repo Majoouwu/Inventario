f99913e020a8499f711a9e41f391d49933cdf377
Rodsote <95106582+Rodsote@users.noreply.github.com>
2023-01-23 23:58:42 -0600

    Modified files

--- src/gui/Vistas/VentanaAdmin.java
+++ src/gui/Vistas/VentanaAdmin.java
@@ -1,24 +1,23 @@
-package src.gui;
+package src.gui.Vistas;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class VentanaAdmin{
+        JFrame ventana = new JFrame();
+        JPanel contentPane = (JPanel) ventana.getContentPane();
+public VentanaAdmin(){
 
-    JFrame ventana = new JFrame();
-    JPanel contentPane = (JPanel) ventana.getContentPane();
-    public VentanaAdmin(){
+        }
 
-    }
-
-    public void mostrar(){
+public void mostrar(){
         crearVentana();
         ponerEtiquetas();
         ponerBotones();
 
-    }
+        }
 
-    private void crearVentana(){
+private void crearVentana(){
         ventana.setSize(1280,720);
         ventana.setVisible(true);
         ventana.setResizable(false);
@@ -27,9 +26,9 @@ public class VentanaAdmin{
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+        }
 
-    private void ponerEtiquetas(){
+private void ponerEtiquetas(){
         JLabel etiqueta = new JLabel();
         contentPane.add(etiqueta);
         etiqueta.setText("Inventario: Majo Y Rodrigo S.A. de C.V.");
@@ -37,9 +36,9 @@ public class VentanaAdmin{
         etiqueta.setFont(new Font("Sans", Font.PLAIN,35));
 
 
-    }
+        }
 
-    private void ponerBotones(){
+private void ponerBotones(){
         JButton boton = new JButton();
         contentPane.add(boton);
         boton.setBounds(20,50,100,30);
@@ -49,6 +48,5 @@ public class VentanaAdmin{
         JButton boton3 = new JButton();
         contentPane.add(boton3);
         boton3.setBounds(0,250,100,30);
-    }
-
+        }
 }
